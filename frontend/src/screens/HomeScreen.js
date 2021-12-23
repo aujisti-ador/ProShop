241dfb9f3335ef772f5f44bf4807f14a486ac5e0
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2021-12-23 17:36:12 +0600

    backend api call integrated from frontend in HomeScreen and ProductScreen page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+            // console.log(data)
+            setProducts(data)
+        }
+
+        fetchProducts()
+        // console.log(products)
+    }, [])
+
     return (
         <>
             <h1>Latest Products</h1>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 
 const ProductScreen = () => {
 
-    // console.log('Here --->' + match)
     let { id } = useParams()
-    const product = products.find((p) => p._id === id)
-    // console.log('Here --->' + product)
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${id}`)
+            // console.log(data)
+            setProduct(data)
+        }
+
+        fetchProduct()
+    }, [])
 
     return (
         <>
