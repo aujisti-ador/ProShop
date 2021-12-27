8511a612cd6998409d7338886ebada0f51505c7a
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2021-12-27 23:35:11 +0600

    Redux added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter
 } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
