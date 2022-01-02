af5f355f72aec0760ee64278a7ad3bf175528f1f
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2022-01-02 12:38:20 +0600

    Created cart screen and productScreen updated with url parameter.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import Message from './components/Message';
 
 function App() {
   return (
@@ -18,6 +20,14 @@ function App() {
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart' element={<CartScreen />} />
+            <Route path='/cart/:id' element={<CartScreen />} />
+            <Route
+              path="*"
+              element={
+                <Message variant='danger'>There's nothing here!</Message>
+              }
+            />
           </Routes>
         </Container>
       </main>
