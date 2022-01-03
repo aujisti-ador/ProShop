2a7ab6dcb46611ce7a26e48bca3614e3059b3ef8
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2022-01-03 16:33:40 +0600

    cartScreen updated with dispatching action

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useLocation, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartAction'
 
 function CartScreen() {
+    const { id: productId } = useParams()
+    let location = useLocation().search
+    const qty = location ? Number(location.split('=')[1]) : 1
+    // console.log(`productID: ${id} || location: ${location}`)
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+    console.log(cartItems)
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
+
     return (
         <div>
             Cart
